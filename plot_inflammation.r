6a0aa50a3bc084e62a5ea2b6735907ab45c199d2
Hannah Holland-Moritz <hhollandmoritz@gmail.com>
2014-06-17 11:29:42 -0700

    First version of our r script plot_inflammation.r

---
+++ plot_inflammation.r
@@ -0,0 +1,30 @@
+# Author: Naupaka Zimmerman June 16, 2014
+# Script to make figure with ggplot from
+# command line argument for SWC bootcamp
+# UC Davis - Davis, CA
+ 
+#.libPaths("C:/Users/SDSC/Documents/R/win-library/3.1")
+ 
+# Load libraries
+require(ggplot2)
+require(reshape2)
+ 
+# Takes one command line argument as input data set
+#allows to get command-line arguments
+options <- commandArgs(trailingOnly = TRUE)
+ 
+# load that csv from command line arg
+# options[1] is first option specified in command line aka your .csv file
+data.in <- read.csv(options[1])
+ 
+# melt with defaults
+data.in.melted <- melt(data.in)
+ 
+out.plot <- ggplot(data.in.melted,(aes(x=PatientID, y=value))) + 
+    geom_point() + 
+    stat_sum() +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+	
+# save pdf based on input file name
+savename <- paste0(options[1],".pdf")
+ggsave(out.plot, file=savename, width = 12, height = 8)
